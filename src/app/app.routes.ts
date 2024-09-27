97751d35122f62232c29f7ba571f0c76ba0041ab
alanjacob-exe <alan.abin9388@gmail.com>
2024-09-27 18:54:49 +0530

    adds thankyou page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
+import { ExitComponent } from './pages/exit/exit.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'movies', component: MoviesComponent },
+  { path: 'thank-you', component: ExitComponent },
 ];
