7e6cff6b170331531f9a7233a697b3daf4262993
alanjacob-exe <alan.abin9388@gmail.com>
2024-09-27 17:01:11 +0530

    adds address form

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-address',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './address.component.html',
   styleUrl: './address.component.sass',
 })
-export class AddressComponent {}
+export class AddressComponent {
+  @Input() form!: FormGroup;
+
+  addressFields: { label: string; controlName: string; placeHolder: string }[] =
+    [
+      { label: 'Address 1', placeHolder: 'Address 1', controlName: 'address1' },
+      { label: 'Address 2', placeHolder: 'Address 2', controlName: 'address2' },
+    ];
+
+  locationFields: {
+    label: string;
+    controlName: string;
+    placeHolder: string;
+  }[] = [
+    { label: 'State', placeHolder: 'State', controlName: 'state' },
+    { label: 'Zip Code', placeHolder: 'Zip Code', controlName: 'zip' },
+  ];
+}
