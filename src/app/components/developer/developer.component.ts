3b2c0d46c7065a27eb2e8a09254d3279b2017d88
alanjacob-exe <alan.abin9388@gmail.com>
2024-09-27 17:01:27 +0530

    adds developer form connection

--- src/app/components/developer/developer.component.ts
+++ src/app/components/developer/developer.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-developer',
   standalone: true,
-  imports: [DeveloperComponent, MatRadioModule],
+  imports: [
+    DeveloperComponent,
+    MatRadioModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './developer.component.html',
   styleUrl: './developer.component.sass',
 })
-export class DeveloperComponent {}
+export class DeveloperComponent implements OnInit {
+  @Input() form!: FormGroup;
+
+  ngOnInit(): void {}
+
+  radioValues: { label: string; value: string }[] = [
+    { value: '0-3 years', label: '0-3 years' },
+    { value: '4-6 years', label: '4-6 years' },
+    { value: '7 or more years', label: '7 or more years' },
+  ];
+}
